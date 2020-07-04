056b136ec2b2bce2f77d24fe6b6414a47590cd20
Julius Muschaweck <julius@zs66.de>
2020-07-04 08:58:57 +0200

    Fixed bug in AddSpectra.m where two spectra with different additional fields could not be added

--- AddSpectra.m
+++ AddSpectra.m
@@ -2,5 +2,5 @@ function rv = AddSpectra(lhs, rhs)
 % function rv = AddSpectra(lhs, rhs)
 % input: lhs, rhs: spectra. i.e. struct with vector fields lam and val
 % output: sum of both. 
-    rv = AddWeightedSpectra([lhs rhs],[1 1]);
+    rv = AddWeightedSpectra({lhs rhs},[1 1]);
 end
